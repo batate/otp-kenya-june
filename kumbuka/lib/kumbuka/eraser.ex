81abeb117616432c2d3a794f18ff110bd526b934
pops <76pollet@gmail.com>
2020-07-28 11:34:52 +0300

    the erase function replaces some characters of the initial string with underscores

--- kumbuka/lib/kumbuka/eraser.ex
+++ kumbuka/lib/kumbuka/eraser.ex
@@ -1,18 +1,45 @@
 defmodule Kumbuka.Eraser do
   defstruct [:text, :steps]
+
   def new(text, no_of_steps) do
     __struct__(text: text, steps: build_steps(text, no_of_steps))
   end
+
   defp build_steps(text, no_of_steps) do
     size = String.length(text)
-    (1..size)
-    |> Enum.shuffle
-    |> Enum.chunk_every(step_size(no_of_steps,size))
 
+    1..size
+    |> Enum.shuffle()
+    |> Enum.chunk_every(step_size(no_of_steps, size))
   end
+
   defp step_size(no_of_steps, size) do
     size
     |> Kernel./(no_of_steps)
     |> ceil
+  end
+
+  def erase(acc) do
+    current_step = acc.steps |> step()
+
+    acc.text
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.map(replace(current_step))
+    |> Enum.join()
+  end
+
+  defp step([current_step | _schedule]) do
+    current_step
+  end
+
+  defp replace(current_step) do
+    fn {char, index} ->
+      if index in current_step do
+        "_"
+      else
+        char
+      end
+    end
   end
 end
